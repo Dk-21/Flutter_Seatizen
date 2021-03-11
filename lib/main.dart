88c1a5dafd13af59aa41f14b8d7a77585c3a12e8
Denish <denishkalariya21@icloud.com>
2021-03-11 21:33:30 +0530

    1

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SeatiZen',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(title: Text('titlePage')),
+      body: MyStatelessWidget(),
+    );
+  }
+}
+/// This is the stateless widget that the main application instantiates.
+class MyStatelessWidget extends StatelessWidget {
+  MyStatelessWidget({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return DataTable(
+      columns: const <DataColumn>[
+        DataColumn(
+          label: Text(
+            'Bus_Id',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            'No_of_Steps',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            'Up-Down',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+      ],
+      rows: const <DataRow>[
+
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('BRTS 1')),
+            DataCell(Text('4')),
+            DataCell(Text('25-30')),
+          ],
+        ),
+        
+      ],
+    );
+  }
+}
